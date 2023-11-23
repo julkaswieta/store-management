8817d9e6356bdb608031f84be14bcd8e0f4d3bc7
Julia Swietochowska <julkaswietochowska@gmail.com>
2023-11-23 10:48:49 +0000

    finish reports page

--- ClientApp/ClientApp/PriceControlPage.xaml.cs
+++ ClientApp/ClientApp/PriceControlPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Net.Http;
+using System.Text;
 using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
@@ -34,24 +35,36 @@ namespace ClientApp
         {
             using (HttpClient client = new HttpClient())
             {
-                var options = new JsonSerializerOptions
+                try
                 {
-                    PropertyNameCaseInsensitive = true
-                };
-                var response = await client.GetStringAsync(PriceControlAPI);
-                items = JsonSerializer.Deserialize<ObservableCollection<Item>>(response, options)!;
-                foreach (var item in items) { item.PropertyChanged += Item_PropertyChanged; }
-                dgItems.ItemsSource = items;
+                    var options = new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    };
+                    var response = await client.GetStringAsync(PriceControlAPI);
+                    items = JsonSerializer.Deserialize<ObservableCollection<Item>>(response, options)!;
+                    foreach (var item in items) { item.PropertyChanged += Item_PropertyChanged; }
+                    dgItems.ItemsSource = items;
+                }
+                catch (HttpRequestException ex)
+                {
+                    MessageBox.Show("Not connected to the Price Control service");
+                }
             }
         }
 
-        private void Item_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        private async void Item_PropertyChanged(object sender, PropertyChangedEventArgs e)
         {
             var item = sender as Item;
-            MessageBox.Show("Price changed to: " + item.Price);
+            using (HttpClient client = new HttpClient())
+            {
+                string url = PriceControlAPI + "/" + item.Id;
+                string json = JsonSerializer.Serialize<Item>(item);
+                HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PutAsync(url, content);
+            }
         }
-
-
     }
 
     public partial class Item : INotifyPropertyChanged
--- ClientApp/ClientApp/ReportsPage.xaml.cs
+++ ClientApp/ClientApp/ReportsPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Net.Http;
+using System.Windows.Controls;
 
 namespace ClientApp
 {
@@ -7,6 +8,8 @@ namespace ClientApp
     /// </summary>
     public partial class ReportsPage : Page
     {
+        private readonly string ReportsApiUrlBase = "http://localhost:3007/reports";
+        private string report;
         public ReportsPage()
         {
             InitializeComponent();
@@ -15,6 +18,42 @@ namespace ClientApp
         private void btnBack_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             this.NavigationService.GoBack();
+        }
+
+        private void btnToday_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            GetReport("today");
+        }
+
+        private async void GetReport(string option)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string url = ReportsApiUrlBase + "/" + option;
+                var response = await client.GetStringAsync(url);
+                if (response != null)
+                {
+                    report = response;
+                    report = report.Substring(1, report.Length - 2);
+
+                    txtReport.Text = report;
+                }
+            }
+        }
+
+        private void btnWeek_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            GetReport("weekly");
+        }
+
+        private void btnMonth_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            GetReport("monthly");
+        }
+
+        private void btnYear_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            GetReport("yearly");
         }
     }
 }
